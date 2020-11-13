7b87bb3498f55190a5b6e3efd026ae68f208fd9c
wadthyk <66349360+wadthyk@users.noreply.github.com>
2020-11-13 18:29:50 +0900

    Update MainScreenView with Navigation View

--- Mind Math App/MainScreenView.swift
+++ Mind Math App/MainScreenView.swift
@@ -9,9 +9,21 @@ import SwiftUI
 
 struct MainScreenView: View {
     var body: some View {
+        NavigationView{
+            NavigationLink(destination: Text("In Line Menu (Second View)")) {
+                VStack(spacing: 30){
         VStack {
-            Text("This is the MainScreen")
             Text("Welcome").font(.title).bold()
+                .navigationBarTitle("Navigation")
+            Text("This is the MainScreen")
+                }
+        VStack {
+            Text("Options").font(.title).bold()
+                .navigationBarTitle("Navigation")
+            Text("Click Button for Options View")
+                    }
+                }
+            }
         }
     }
 }
