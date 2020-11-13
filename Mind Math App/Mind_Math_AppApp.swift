520ae6d0c77ff4e7ec01603847d9338152523d4d
wadthyk <66349360+wadthyk@users.noreply.github.com>
2020-11-13 16:40:16 +0900

    Initial Commit

---
+++ Mind Math App/Mind_Math_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mind_Math_AppApp.swift
+//  Mind Math App
+//
+//  Created by Vongwadthy Khieu on 13.11.20.
+//
+
+import SwiftUI
+
+@main
+struct Mind_Math_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
