ec924e4b8eedb495dd8159a55c2fe0e923f04e40
wadthyk <66349360+wadthyk@users.noreply.github.com>
2020-11-13 17:20:18 +0900

    Import of App Logic Math Operations

--- Mind Math App/GameLogic.swift
+++ Mind Math App/GameLogic.swift
@@ -6,3 +6,45 @@
 //
 
 import Foundation
+
+
+func makeQuestionAddition() {
+    let randomNumber1 = Int.random(in: 1...99)
+    let randomNumber2 = Int.random(in: 1...99)
+    print("\(randomNumber1) + \(randomNumber2)")
+    let solution = randomNumber1 + randomNumber2
+    print("The Solution is \(solution)." )
+}
+
+func makeQuestionSubtraction() {
+    let randomNumber1 = Int.random(in: 1...99)
+    let randomNumber2 = Int.random(in: 1...randomNumber1)
+    print("\(randomNumber1) - \(randomNumber2)")
+    let solution = randomNumber1 - randomNumber2
+    print("The Solution is \(solution)." )
+}
+
+func makeQuestionMultiplication() {
+    let randomNumber1 = Int.random(in: 1...99)
+    let randomNumber2 = Int.random(in: 1...10)
+    print("\(randomNumber1) x \(randomNumber2)")
+    let solution = randomNumber1 * randomNumber2
+    print("The Solution is \(solution)." )
+}
+
+func makeQuestionDivision() {
+    let randomNumber1 = Int.random(in: 1...99)
+    let randomNumber2 = Int.random(in: 1...randomNumber1)
+    let questionNumber = randomNumber1 % randomNumber2
+    if randomNumber1 % randomNumber2 == 0 {
+        print(randomNumber2)
+    } else {
+        print("Number doesn't work for this operation")
+    }
+    print("\(randomNumber1) / \(randomNumber2)")
+    let solution = randomNumber1 / randomNumber2
+    print("The Solution is \(solution)." )
+    print("\(questionNumber) are left.")
+}
+
+
