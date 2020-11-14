e925792b3e46150b0bae39c5e9a85bf2fa89568b
wadthyk <66349360+wadthyk@users.noreply.github.com>
2020-11-14 16:18:08 +0900

    Alignment of Submenu plus Icons

--- Mind Math App/MainScreenView.swift
+++ Mind Math App/MainScreenView.swift
@@ -11,16 +11,21 @@ struct MainScreenView: View {
     var body: some View {
         NavigationView{
             NavigationLink(destination: Text("In Line Menu (Second View)")) {
-                VStack(spacing: 30){
-        VStack {
+                VStack(alignment: .leading) {
+        HStack {
+            Image(systemName: "house.fill")
             Text("Welcome").font(.title).bold()
                 .navigationBarTitle("Navigation")
-            Text("This is the MainScreen")
                 }
-        VStack {
-            Text("Options").font(.title).bold()
+        HStack {
+            Image(systemName: "play.circle.fill")
+            Text("Play").font(.title).bold()
                 .navigationBarTitle("Navigation")
-            Text("Click Button for Options View")
+                    }
+        HStack {
+            Image(systemName: "gearshape.fill")
+            Text("Settings").font(.title).bold()
+                .navigationBarTitle("Navigation")
                     }
                 }
             }
